c4205cfe3ca75aa39a3f4a2d86400a2f0a9512f3
Skyler Malinowski <skyler@schedmd.com>
2024-11-11 13:56:18 -0500

    chore: extend type-enum rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,23 @@
 export default {
   extends: [
     '@commitlint/config-conventional',
-  ]
+  ],
+  rules: {
+    "type-enum": [2, 'always', [
+        'build',
+        'changelog',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'reapply',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
