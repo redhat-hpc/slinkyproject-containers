redhat-hpc/slinkyproject-containers

commit 42b95c36266f66f285e33688110b03092bfa52c0

refactor: migrate to authselect

schedmd/slurm/24.11/rockylinux9/Dockerfile
@@ -395,11 +395,16 @@ dnf -q makecache --refresh
 dnf -q -y install --setopt='install_weak_deps=False' \
   openssh-server \
   dbus-daemon \
-  authconfig sssd sssd-ad sssd-ldap sssd-dbus
+  authselect sssd sssd-ad sssd-ldap sssd-dbus
 rm *.rpm && dnf clean all
 # Configure
-authconfig --enablemkhomedir --updateall
+mkdir -p /etc/authselect
+authselect select sssd with-mkhomedir --force
 rm -f /etc/ssh/ssh_host_*
+# Remove Packages
+dnf -q -y remove \
+  authselect
+dnf -q -y autoremove
 EOR
 
 COPY files/login/ /

schedmd/slurm/24.11/ubuntu24.04/Dockerfile
@@ -410,11 +410,16 @@ apt-get -qq update
 apt-get -qq -y install --no-install-recommends \
   openssh-server \
   dbus-daemon \
-  sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
+  authselect sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
 rm *.deb && apt-get clean && rm -rf /var/lib/apt/lists/*
 # Configure
-pam-auth-update --enable mkhomedir --force
+mkdir -p /etc/authselect
+authselect select sssd with-mkhomedir --force
 rm -f /etc/ssh/ssh_host_*
+# Remove Packages
+apt-get -qq -y remove \
+  authselect
+apt-get -qq -y autoremove
 EOR
 
 COPY files/login/ /

schedmd/slurm/master/rockylinux9/Dockerfile
@@ -395,11 +395,16 @@ dnf -q makecache --refresh
 dnf -q -y install --setopt='install_weak_deps=False' \
   openssh-server \
   dbus-daemon \
-  authconfig sssd sssd-ad sssd-ldap sssd-dbus
+  authselect sssd sssd-ad sssd-ldap sssd-dbus
 rm *.rpm && dnf clean all
 # Configure
-authconfig --enablemkhomedir --updateall
+mkdir -p /etc/authselect
+authselect select sssd with-mkhomedir --force
 rm -f /etc/ssh/ssh_host_*
+# Remove Packages
+dnf -q -y remove \
+  authselect
+dnf -q -y autoremove
 EOR
 
 COPY files/login/ /

schedmd/slurm/master/ubuntu24.04/Dockerfile
@@ -410,11 +410,16 @@ apt-get -qq update
 apt-get -qq -y install --no-install-recommends \
   openssh-server \
   dbus-daemon \
-  sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
+  authselect sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
 rm *.deb && apt-get clean && rm -rf /var/lib/apt/lists/*
 # Configure
-pam-auth-update --enable mkhomedir --force
+mkdir -p /etc/authselect
+authselect select sssd with-mkhomedir --force
 rm -f /etc/ssh/ssh_host_*
+# Remove Packages
+apt-get -qq -y remove \
+  authselect
+apt-get -qq -y autoremove
 EOR
 
 COPY files/login/ /

commit 79a0ba4635b221bce4d454c082225ccef2867ef0

fix: handle mkhomedir with authselect config
Regression caused by commit 42b95c3.

schedmd/slurm/24.11/rockylinux9/Dockerfile
@@ -291,6 +291,7 @@ set -xeuo pipefail
 dnf -q -y install --setopt='install_weak_deps=False' \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus
 rm *.rpm && dnf clean all
 # Configure

schedmd/slurm/24.11/rockylinux9/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/system.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/24.11/rockylinux9/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {

schedmd/slurm/24.11/ubuntu24.04/Dockerfile
@@ -312,6 +312,7 @@ apt-get -qq update
 apt-get -qq -y install --no-install-recommends \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
 rm *.deb && apt-get clean && rm -rf /var/lib/apt/lists/*
 # Configure

schedmd/slurm/24.11/ubuntu24.04/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/session.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/24.11/ubuntu24.04/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {

schedmd/slurm/25.05/rockylinux9/Dockerfile
@@ -291,6 +291,7 @@ set -xeuo pipefail
 dnf -q -y install --setopt='install_weak_deps=False' \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus
 rm *.rpm && dnf clean all
 # Configure

schedmd/slurm/25.05/rockylinux9/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/system.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/25.05/rockylinux9/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {

schedmd/slurm/25.05/ubuntu24.04/Dockerfile
@@ -312,6 +312,7 @@ apt-get -qq update
 apt-get -qq -y install --no-install-recommends \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
 rm *.deb && apt-get clean && rm -rf /var/lib/apt/lists/*
 # Configure

schedmd/slurm/25.05/ubuntu24.04/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/session.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/25.05/ubuntu24.04/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {

schedmd/slurm/master/rockylinux9/Dockerfile
@@ -291,6 +291,7 @@ set -xeuo pipefail
 dnf -q -y install --setopt='install_weak_deps=False' \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus
 rm *.rpm && dnf clean all
 # Configure

schedmd/slurm/master/rockylinux9/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/system.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/master/rockylinux9/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {

schedmd/slurm/master/ubuntu24.04/Dockerfile
@@ -310,6 +310,7 @@ apt-get -qq update
 apt-get -qq -y install --no-install-recommends \
   openssh-server \
   dbus-daemon \
+  oddjob-mkhomedir \
   authselect sssd sssd-ad sssd-ldap sssd-dbus libpam-sss libnss-sss
 rm *.deb && apt-get clean && rm -rf /var/lib/apt/lists/*
 # Configure

schedmd/slurm/master/ubuntu24.04/files/login/etc/supervisor/supervisord.conf
@@ -38,6 +38,15 @@ stopasgroup=true
 startsecs=5
 priority=2
 
+[program:oddjobd]
+command=bash -xc "/usr/sbin/oddjobd -n -p /run/oddjobd.pid -t 300 %(ENV_ODDJOB_OPTIONS)s"
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+stopasgroup=true
+priority=2
+
 [program:dbus]
 command=bash -xc "exec dbus-daemon --config-file=/usr/share/dbus-1/session.conf --nofork --nosyslog %(ENV_DBUS_OPTIONS)s"
 stdout_logfile=/dev/stdout

schedmd/slurm/master/ubuntu24.04/files/login/usr/local/bin/entrypoint.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export ODDJOB_OPTIONS="${ODDJOB_OPTIONS:-""}"
 export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 function main() {